ec63b70f1a5c15d9dedd995a1429473623edc5ac
Swapnil vishwakarma <swapnilsharma881@gmail.com>
2024-05-16 01:54:16 +0530

    Routing Solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Profile from "./pages/Profile";
-import Home from "./pages/Home";
-
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import About from './pages/About';
 export default function App() {
   return <BrowserRouter>
        <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/sign-in" element={<SignIn/>} />
-        <Route path="/sign-out" element={<SignUp/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/profile" element={<Profile/>} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
        </Routes>
   
   </BrowserRouter>;
-
+  
 };
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function SignOut() {
+export default function SignUp() {
   return (
-    <div>SignOut</div>
+    <div>SignUp</div>
   )
 }
